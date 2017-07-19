e01a63c316ae2771ab5d8f99249a6dfc2dce2813
Ryan McCrary <rrmccrary@gmail.com>
2017-07-19 14:53:54 -0400

    Add auth0 controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'auth0/callback'
+
+  get 'auth0/failure'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
